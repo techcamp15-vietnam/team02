7b40580434c943c3486d2fe249baf5ac74e40eb0
thangnh0409 <ht.hedspi.bk@gmail.com>
2014-01-07 11:33:54 +0700

    create main layout

---
+++ gen/com/shinkansen/touchcolor/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.shinkansen.touchcolor;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/shinkansen/touchcolor/MainActivity.java
+++ src/com/shinkansen/touchcolor/MainActivity.java
@@ -1,8 +1,9 @@
 package com.shinkansen.touchcolor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,28 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/**
+	 Event when button click
+	@param view : view of button 
+	@author huuthang
+	*/
+	public void onButtonClick(View view) {
+		switch (view.getId()) {
+		case R.id.btnMenu:
+			
+			break;
+		case R.id.btnNextColor:
+			
+			break;
+		case R.id.btnPlay:
+			
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
