5cf18a96445dfadfbf5fb90a9e4e3aab59930dd9
thangnh0409 <ht.hedspi.bk@gmail.com>
2014-01-07 18:20:36 +0700

    Add Slide Image

--- src/com/shinkansen/touchcolor/MainActivity.java
+++ src/com/shinkansen/touchcolor/MainActivity.java
@@ -1,16 +1,59 @@
 package com.shinkansen.touchcolor;
 
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Gallery;
+import com.shinkansen.touchcolor.adapter.ImageAdapter;
+import com.shinkansen.touchcolor.adapter.ImageViewPagerAdapter;
+import com.shinkansen.touchcolor.datahelper.RelateObjectDataSource;
 
 public class MainActivity extends Activity {
+	
+	private RelateObjectDataSource relateObject;
+	Integer[] pics = {
+    		R.drawable.active1,
+    		R.drawable.active2,
+    		R.drawable.frame_white
+    };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		relateObject = new RelateObjectDataSource(this);
+		
+		/**
+		 * test add data
+		 * 
+		RelateObject testObj = new RelateObject();
+		testObj.setRObjId(2);
+		testObj.setObjectColor("Blue");
+		testObj.setObjectName("Ball");
+		testObj.setObjectImageName("ball");
+		
+		relateObject.addRelateObject(testObj);*/
+		
+		
+		Gallery gallery = (Gallery) findViewById(R.id.gallery);
+		gallery.setAdapter(new ImageAdapter(this, pics));
+		gallery.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				showPopupImage(pics[arg2], arg2);
+			}
+        	
+        });
 	}
 
 	@Override
@@ -41,5 +84,27 @@ public class MainActivity extends Activity {
 			break;
 		}
 	}
+	
+	/**
+	 Show popup Image
+	@param imageResource: id of image; position: index of image
+	@author huuthang
+	*/
+	private void showPopupImage(Integer imageResource, int position){
+    	AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+    	LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+    	View layout = inflater.inflate(R.layout.custom_popup_image, (ViewGroup) findViewById(R.id.layout_root));
+   
+    	
+    	ViewPager viewPager = (ViewPager) layout.findViewById(R.id.view_pager);
+    	viewPager.setAdapter(new ImageViewPagerAdapter(this, pics));
+    	viewPager.setCurrentItem(position);
+    	dialog.setView(layout);
+    	dialog.setCancelable(true);
+    	
+    	dialog.create();
+    	dialog.show();
+    	
+    }
 
 }
