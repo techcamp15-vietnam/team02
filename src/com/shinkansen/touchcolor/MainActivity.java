539ce023e2c42ae7b2818826f9ebc6f268d6af18
HCT <bubiwin@gmail.com>
2014-01-07 16:22:01 +0700

    Add background.png Modify ActivityMain.java

--- src/com/shinkansen/touchcolor/MainActivity.java
+++ src/com/shinkansen/touchcolor/MainActivity.java
@@ -1,9 +1,13 @@
 package com.shinkansen.touchcolor;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +22,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.action_about:
+			showDialogAbout();
+        return true;
+		case R.id.action_manual:
+			showDialogManual();
+        return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -41,5 +57,40 @@ public class MainActivity extends Activity {
 			break;
 		}
 	}
+	/**
+	 Menu show dialog
+	@param
+	@author congthang
+	*/
+	public void showDialogAbout(){
+		AlertDialog.Builder b=new AlertDialog.Builder(MainActivity.this);
+		 
+		b.setTitle("About");
+		b.setMessage("Sinkansen team");
+		b.setPositiveButton("OK", new DialogInterface. OnClickListener() {
+		@Override
+		public void onClick(DialogInterface dialog, int which)
+		{
+			dialog.cancel();
+		}});
 
+		b.create().show();
+	}
+	public void showDialogManual(){
+	AlertDialog.Builder b=new AlertDialog.Builder(MainActivity.this);
+	 
+	b.setTitle("Manual");
+	b.setMessage("I don't know!");
+	b.setPositiveButton("OK", new DialogInterface. OnClickListener() {
+	@Override
+	public void onClick(DialogInterface dialog, int which)
+	{
+		dialog.cancel();
+	}});
+
+	b.create().show();
+}
+	
+	
+	
 }
