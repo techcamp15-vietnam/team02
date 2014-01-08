7c1cff3bdc280724a43607e607380edb75db8900
Thanhbk <dinhduythanh51292@gmail.com>
2014-01-08 22:22:34 +0700

    Set constant value of color

--- src/com/shinkansen/touchcolor/constant/Constant.java
+++ src/com/shinkansen/touchcolor/constant/Constant.java
@@ -1,6 +1,8 @@
 package com.shinkansen.touchcolor.constant;
 
+import android.graphics.Color;
+
 public class Constant {
-	public static final String[] COLOR = {"red"};
-	public static final Integer[] COLOR_ID = {-65536};
+	public static final String[] COLOR = {"red","blue","green","yellow","orange","purple","pink","white","black"};
+	public static final Integer[] COLOR_ID = {Color.parseColor("#FF0000"),Color.parseColor("#0000FF"),Color.parseColor("#008000"),Color.parseColor("#FFFF00"),Color.parseColor("#FFA500"),Color.parseColor("#800080"),Color.parseColor("#FFC0CB"),Color.parseColor("#FFFFFF"), Color.parseColor("#000000")};
 }
