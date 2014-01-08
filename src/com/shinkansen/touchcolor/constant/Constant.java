8f2c995366aef3d0cdc05107ffad48797532758d
thangnh0409 <ht.hedspi.bk@gmail.com>
2014-01-08 23:02:11 +0700

    changes constant.java

--- src/com/shinkansen/touchcolor/constant/Constant.java
+++ src/com/shinkansen/touchcolor/constant/Constant.java
@@ -1,6 +1,10 @@
 package com.shinkansen.touchcolor.constant;
 
+import com.shinkansen.touchcolor.R;
+
+
 public class Constant {
-	public static final String[] COLOR = {"red"};
-	public static final Integer[] COLOR_ID = {-65536};
+	public static final String[] COLOR = {"red", "yellow"};
+	public static final Integer[] COLOR_ID = {-65536, -256};
+	public static final Integer[] SOUND_ID = {R.raw.red, R.raw.yellow};
 }
