f52456e460cb874bd50636968480a1397866249b
thangnh0409 <ht.hedspi.bk@gmail.com>
2014-01-08 08:54:08 +0700

    Add preview for take photo

--- gen/com/shinkansen/touchcolor/R.java
+++ gen/com/shinkansen/touchcolor/R.java
@@ -30,33 +30,38 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_about=0x7f08000c;
-        public static final int action_manual=0x7f08000d;
-        public static final int btnMenu=0x7f080004;
-        public static final int btnNextColor=0x7f080003;
+        public static final int action_about=0x7f08000d;
+        public static final int action_manual=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
+        public static final int btnMenu=0x7f080008;
+        public static final int btnNextColor=0x7f080004;
         public static final int btnPlay=0x7f080006;
-        public static final int button1=0x7f08000b;
-        public static final int button2=0x7f08000a;
+        public static final int button1=0x7f08000a;
+        public static final int button2=0x7f080009;
         public static final int gallery=0x7f080007;
-        public static final int imageView2=0x7f080001;
-        public static final int ivBackground=0x7f080000;
+        public static final int ivBackground=0x7f080001;
+        public static final int ivShowColor=0x7f080002;
         public static final int ivTransparent=0x7f080005;
-        public static final int layout_root=0x7f080008;
-        public static final int txtColorName=0x7f080002;
-        public static final int view_pager=0x7f080009;
+        public static final int layout_root=0x7f08000b;
+        public static final int preview=0x7f080000;
+        public static final int txtColorName=0x7f080003;
+        public static final int view_pager=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int custom_popup_image=0x7f030001;
-        public static final int play=0x7f030002;
+        public static final int activity_play=0x7f030001;
+        public static final int custom_popup_image=0x7f030002;
+        public static final int play=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int play=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_play=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/shinkansen/touchcolor/MainActivity.java
+++ src/com/shinkansen/touchcolor/MainActivity.java
@@ -4,34 +4,80 @@ package com.shinkansen.touchcolor;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.FrameLayout;
 import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.shinkansen.touchcolor.adapter.ImageAdapter;
 import com.shinkansen.touchcolor.adapter.ImageViewPagerAdapter;
 import com.shinkansen.touchcolor.datahelper.RelateObjectDataSource;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private Preview mPreview;
+	private Camera mCamera;
+	public int x, y;
+	public static Point size,scale;
+	private TextView txtColorName;
+	private ImageView ivShowColor;
+	private ImageView ivTransparent;
+	private FrameLayout previewLayout;
 	
 	private RelateObjectDataSource relateObject;
 	Integer[] pics = {
     		R.drawable.active1,
     		R.drawable.active2,
-    		R.drawable.frame_white
+    		R.drawable.ic_launcher
     };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ivTransparent = (ImageView) findViewById(R.id.ivTransparent);
+		
+		previewLayout = (FrameLayout) findViewById(R.id.preview);
+		txtColorName = (TextView) findViewById(R.id.txtColorName);
+		ivShowColor = (ImageView) findViewById(R.id.ivShowColor);
+		
+		//init camera
+		mPreview = new Preview(this);
+		mCamera = Camera.open();
+		mPreview.setCamera(mCamera);
+		previewLayout.addView(mPreview);
+		
+		ivTransparent.setOnTouchListener(ontch);
+		
+		
+		String colorString = dummydata();
+		
+		ivShowColor.setBackgroundColor(Color.parseColor(colorString));
+		txtColorName.setText(colorString);
+		txtColorName.setTextColor(Color.parseColor(colorString));
+		
 		relateObject = new RelateObjectDataSource(this);
 		
 		/**
@@ -55,15 +101,101 @@ public class MainActivity extends Activity {
 					long arg3) {
 				showPopupImage(pics[arg2], arg2);
 			}
-        	
         });
 	}
+	
+
+	OnTouchListener ontch = new OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View view, MotionEvent event) {
+			// TODO Auto-generated method stub
+			x = (int)event.getX();
+			y = (int)event.getY();
+			
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				//Display display = getWindowManager().getDefaultDisplay();
+				//size = new Point();
+				//display.getSize(size);
+				mPreview.takePicture();
+				
+				break;
+			/*case MotionEvent.ACTION_UP:
+				Log.d("aaaaaaaaa", Preview.bitmap.getWidth()+"/"+Preview.bitmap.getHeight());
+				Bitmap bmp = rotateImage(Preview.bitmap,90);
+
+				
+				float sx =(float) bmp.getWidth()/size.x;
+				float sy =(float) bmp.getHeight()/size.y;
+				x=(int)(x*sx);
+				y=(int)(y*sy);
+				int tch = bmp.getPixel(x, y);
+				txtColorName.setTextColor(tch);
+				break;
+			case MotionEvent.ACTION_MOVE:
+				txtColorName.setText("Toa do:" + x + "/" + y);
+				break;
+				*/
+			}
+
+			return true;
+		}
+	};
+	
+	private Bitmap resizeImage(final Bitmap image) {
+		Bitmap resizedImage = null;		
+		resizedImage = Bitmap.createScaledBitmap(image, MainActivity.size.x,MainActivity.size.y, true);
+		return resizedImage;
+	}
+	
+	public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+	    int width = bm.getWidth();
+	    int height = bm.getHeight();
+	    float scaleWidth = ((float) newWidth) / width;
+	    float scaleHeight = ((float) newHeight) / height;
+	    // CREATE A MATRIX FOR THE MANIPULATION
+	    Matrix matrix = new Matrix();
+	    // RESIZE THE BIT MAP
+	    matrix.postScale(scaleWidth, scaleHeight);
+
+	    // "RECREATE" THE NEW BITMAP
+	    Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+	    return resizedBitmap;
+	}
+	public Bitmap rotateImage(Bitmap src, float degree) {
+	     // create new matrix object
+	     Matrix matrix = new Matrix();
+	     // setup rotation degree
+	     matrix.postRotate(degree);
+	     // return new bitmap rotated using matrix
+	     return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+	 }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Because the Camera object is a shared resource, it's very
+		// important to release it when the activity is paused.
+		if (mCamera != null) {
+			mPreview.setCamera(null);
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Open the default i.e. the first rear facing camera.
+		
+
 	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item){
@@ -76,6 +208,12 @@ public class MainActivity extends Activity {
         return true;
 		}
 		return false;
+	}
+	
+	public String dummydata(){
+		String[] color = {"Red", "Blue", "Green", "White", "Black"};
+		int rand = (int)( Math.random() * 5);
+		return color[rand];
 	}
 	
 	/**
@@ -92,7 +230,9 @@ public class MainActivity extends Activity {
 			
 			break;
 		case R.id.btnPlay:
-			
+			Intent intent = new Intent(this, PlayActivity.class);
+			startActivity(intent);
+			overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
 			break;
 
 		default:
@@ -116,8 +256,10 @@ public class MainActivity extends Activity {
     	dialog.setView(layout);
     	dialog.setCancelable(true);
     	
-    	dialog.create();
-    	dialog.show();
+    	AlertDialog alertDialog = dialog.create();
+    	alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+    	alertDialog.show();
+
     	
     }
 	/**
@@ -140,18 +282,18 @@ public class MainActivity extends Activity {
 		b.create().show();
 	}
 	public void showDialogManual(){
-	AlertDialog.Builder b=new AlertDialog.Builder(MainActivity.this);
-	 
-	b.setTitle("Manual");
-	b.setMessage("I don't know!");
-	b.setPositiveButton("OK", new DialogInterface. OnClickListener() {
-	@Override
-	public void onClick(DialogInterface dialog, int which)
-	{
-		dialog.cancel();
-	}});
-
-	b.create().show();
+		AlertDialog.Builder b=new AlertDialog.Builder(MainActivity.this);
+		 
+		b.setTitle("Manual");
+		b.setMessage("I don't know!");
+		b.setPositiveButton("OK", new DialogInterface. OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which){
+				dialog.cancel();
+			}
+		});
+	
+		b.create().show();
 }
 	
 	
