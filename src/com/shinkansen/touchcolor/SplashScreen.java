d17b5894902d235a7507e1ab2d568631f6fcf195
Thanhbk <dinhduythanh51292@gmail.com>
2014-01-10 01:56:46 +0700

    Add animation to spalsh screen

--- src/com/shinkansen/touchcolor/SplashScreen.java
+++ src/com/shinkansen/touchcolor/SplashScreen.java
@@ -4,18 +4,27 @@ import com.shinkansen.touchcolor.soundmanager.SoundManager;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class SplashScreen extends Activity { 
 	 // Splash screen timer
     private static int SPLASH_TIME_OUT = 5000;
- 
+    private AnimationDrawable animation2;
+	private ImageView imgAni2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spalsh);
-        
+        imgAni2 = (ImageView) findViewById(R.id.logoView);
+		animation2 = (AnimationDrawable) imgAni2.getDrawable();
+		 
+        animation2.setCallback(imgAni2);
+
+        animation2.setVisible(true, true);
+        animation2.start();
         SoundManager.getInstance().initSoundBackground(this);
         SoundManager.getInstance().initSound(this);
  
